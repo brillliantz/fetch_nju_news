8521343d791735b628e4779448da485eab016e62
brillliantz <brillliantz@outlook.com>
2017-02-27 15:47:07 +0800

    Add long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+def readme():
+  with open('README.md') as f:
+    return f.read()
+
 setup(name='fetch_nju_news',
       version='0.1',
       description='Get latest announcements from nju websites, then package them.',
+      long_description=readme(),
       url='http://github.com/brillliantz/fetch_nju_news',
       author='William Liu',
       author_email='brillliantz@outlook.com',
